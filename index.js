dc8bef5743e720affd2fad45070e5f9afc2c9498
panda8288 <80282449+panda8288@users.noreply.github.com>
2026-04-21 23:32:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ const admin = require("firebase-admin");
 // ================= FIREBASE SETUP ================= 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 console.log(serviceAccount.project_id);
-//admin.initializeApp({ credential: admin.credential.cert(serviceAccount), storageBucket: ${serviceAccount.project_id}.appspot.com });
-
-//const bucket = admin.storage().bucket();
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount),storageBucket:"$(serviceAccount.project_id).appspot.com"});
+const bucket = admin.storage().bucket();
 
 // ================= APP SETUP ================= 
 const app = express(); app.use(express.urlencoded({ extended: false }));
