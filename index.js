a3d37a273bc2decf9e3845bf8f7820f47e310c95
panda8288 <80282449+panda8288@users.noreply.github.com>
2026-05-07 14:12:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -111,9 +111,9 @@ if (doneMatch) {
   await client.messages.create({
     from: process.env.TWILIO_WHATSAPP_NUMBER,
     to: `whatsapp:${resUser.rows[0].phone}`,
-    ContentSid: `HXb718e7782862310edab30bbe57d54960`
+    //ContentSid: `HXb718e7782862310edab30bbe57d54960`
     //,ContentVariables: JSON.stringify({1: ticketId})
-    //,body: `Your Ticket *#${ticketId}* is resolved. ✅`
+    ,body: `Your Ticket *#${ticketId}* is resolved. ✅`
   });
 
   twiml.message("Ticket Closed & User notified.");
